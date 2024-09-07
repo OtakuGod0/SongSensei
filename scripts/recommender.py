311014b904b8f276589c5981a646e12836296724
otaku <otakugod0000@gmail.com>
2024-09-07 09:36:32 +0545

    changes

---
+++ scripts/recommender.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import random
+import sys
+
+df = pd.read_csv("sample.csv")
+
+def recommend(*songs):
+    recommended_songs = []
+    for song in songs:
+        try: 
+            # Finding Cluster of the song
+            cluster = df[df['song'] == song]['Cluster'].values[0]
+
+            # Songs of same cluster
+            similar_song = df[df['Cluster'] == cluster]
+
+            recommended_songs.append(similar_song.sample(n = 5)['song'].values) # recommending 5 songs of same cluster
+            
+        except IndexError: # Catch unknown songs
+            print("Song not found in database")
+
+
+    random.shuffle(recommended_songs)
+    return recommended_songs
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 0:
+        recommend(*sys.argv[1:]) # unpacking the argv list and passing to recommend
+    else:
+        print("No argument was passed")
